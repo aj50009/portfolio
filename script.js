2ae6e4bb022604f191f61d2a1ab19e52b403d93f
aj50009 <aj50009@fer.hr>
2019-12-27 15:25:23 +0100

    add separate section for closed source projects

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip(); 
+    $('[data-toggle="tooltip"]').tooltip();
+    let closedSourceProjects = $('#closedSourceProjects');
+    if (closedSourceProjects.length > 0) {
+        closedSourceProjects.on('show.bs.collapse', closedSourceProjects_expand);
+        closedSourceProjects.on('hide.bs.collapse', closedSourceProjects_collapse);
+    }
 });
+
+function closedSourceProjects_expand() {
+    $('#closedSourceProjectsButton').html('Hide closed source projects');
+}
+
+function closedSourceProjects_collapse() {
+    $('#closedSourceProjectsButton').html('Show closed source projects');
+}
