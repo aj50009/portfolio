6283b73753719ccb2eeadfc7f23e82c61b2e2a2a
aj50009 <aj50009@fer.hr>
2019-12-27 17:08:23 +0100

    add another game engine

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
+let closedSourceProjectsButton = null;
+let hideString = 'Hide closed source projects';
+let showString = 'Show closed source projects';
+
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
     let closedSourceProjects = $('#closedSourceProjects');
     if (closedSourceProjects.length > 0) {
+        closedSourceProjectsButton = $('#closedSourceProjectsButton');
         closedSourceProjects.on('show.bs.collapse', closedSourceProjects_expand);
         closedSourceProjects.on('hide.bs.collapse', closedSourceProjects_collapse);
+        let getParameters = window.location.search.substr(1).toLowerCase().split('&');
+        if ((getParameters.length >= 2) && (getParameters[1] == 'expand')) {
+            closedSourceProjects.addClass('show');
+            closedSourceProjectsButton.attr('aria-expanded', 'true');
+            closedSourceProjectsButton.html(hideString);
+        }
+        if (getParameters.length >= 1) {
+            let focusedElement = document.getElementById(getParameters[0] + 'Project');
+            if (focusedElement != null)
+                focusedElement.scrollIntoView();
+        }
     }
 });
 
 function closedSourceProjects_expand() {
-    $('#closedSourceProjectsButton').html('Hide closed source projects');
+    closedSourceProjectsButton.html(hideString);
 }
 
 function closedSourceProjects_collapse() {
-    $('#closedSourceProjectsButton').html('Show closed source projects');
+    closedSourceProjectsButton.html(showString);
 }
